f7ea148778f32e9a6cf212a4ff698a1adbc1ce86
Tobias Kommerell <t.kommerell@hotmail.com>
2016-09-14 10:37:21 +0200

    fix data reading

--- data/data.py
+++ data/data.py
@@ -1,28 +1,38 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
 class Data(object):
-    def __init__(self):
-        self.brand = np.fromfile('BRAND.dat', sep = ',')
-        self.campaign = np.fromfile('CAMPAIGN.dat')
-        self.card = np.fromfile('CARD.dat')
-        self.category = np.fromfile('CATEGORY.dat')
-        self.gdg = np.fromfile('GDG.dat')
-        self.item = np.fromfile('ITEM.dat')
-        self.subcategory = np.fromfile('SUBCATEGORY.dat')
-        self.transaction = np.fromfile('TRANSACTION.dat')
-        self.transaction_item = np.fromfile('TRANSACTION_ITEM.dat')
+  def __init__(self):
+    """
+    construct a new Data object
+    :return:
+    """
+    self._raw_brand = self.__read_dat_files('BRAND.dat')
+    self._raw_campaign = self.__read_dat_files('CAMPAIGN.dat')
+    self._raw_card = self.__read_dat_files('CARD.dat')
+    self._raw_category = self.__read_dat_files('CATEGORY.dat')
+    self._raw_gdg = self.__read_dat_files('GDG.dat')
+    self._raw_item = self.__read_dat_files('ITEM.dat')
+    self._raw_subcategory = self.__read_dat_files('SUBCATEGORY.dat')
+    self._raw_transaction = self.__read_dat_files('TRANSACTION.dat')
+    self._raw_transaction_item = self.__read_dat_files('TRANSACTION_ITEM.dat')
+
+  def __read_dat_files(self, filename):
+    """
+    reads one of the dat files that contain the data
+    :param filename: the filename of the file to be read
+    :return: a list of lists containing all the data by line
+    """
+    data = []
+    with open(filename, 'r') as file:
+      for line in file:
+        l = line.split(",")
+        data.append(map(lambda s: s.rstrip(), l))
+    return data
 
 
 if __name__ == '__main__':
-    mydata = Data()
-    print (mydata.brand)
-
-
-
+  mydata = Data()
+  print (mydata._raw_brand[0])
 
 """plt.hist(brand_data)
 plt.show(brand_data)
 
 brand_data.shape"""
-
