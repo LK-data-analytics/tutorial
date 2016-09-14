b0d2e40ed986e0aae945e1bbf8ccddeb0aed785d
Franziska Lange <Franziska Lange>
2016-09-14 09:59:20 +0200

    initial code commit

---
+++ data/data.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Data(object):
+    def __init__(self):
+        self.brand = np.fromfile('BRAND.dat', sep = ',', dtype = np.dtype([('BrandCode', str),('BrandDescription',str)]))
+        self.campaign = np.fromfile('CAMPAIGN.dat')
+        self.card = np.fromfile('CARD.dat')
+        self.category = np.fromfile('CATEGORY.dat')
+        self.gdg = np.fromfile('GDG.dat')
+        self.item = np.fromfile('ITEM.dat')
+        self.subcategory = np.fromfile('SUBCATEGORY.dat')
+        self.transaction = np.fromfile('TRANSACTION.dat')
+        self.transaction_item = np.fromfile('TRANSACTION_ITEM.dat')
+
+
+if __name__ == '__main__':
+    mydata = Data()
+    print (mydata.brand)
+
+
+
+
+"""plt.hist(brand_data)
+plt.show(brand_data)
+
+brand_data.shape"""
+
